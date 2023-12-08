d5c3da2058e07e3aa3b8f3a7fd71bc54292751b6
Hannes Seidel <greenredact@gmail.com>
2023-12-08 21:55:23 +0100

    refactored sensor state model - combined update methods

--- lib/models/sensor_state_model.dart
+++ lib/models/sensor_state_model.dart
@@ -4,6 +4,8 @@ import 'dart:typed_data';
 import 'package:feet_back_app/models/sensor_device_selector.dart';
 import 'package:feet_back_app/models/sensor_values.dart';
 
+import '../enums/side.dart';
+
 class SensorStateModel {
   static final SensorStateModel _instance = SensorStateModel._internal();
   SensorStateModel._internal();
@@ -63,7 +65,7 @@ class SensorStateModel {
     );
   }
 
-  updateLeft12Values(List<int> data) {
+  updateFsrtecValues(List<int> data, Side side) {
     // assumptions:
     // - package length is always multiple of 2 (List of uint16 big endian)
     // - full package has 28 bytes
@@ -73,108 +75,115 @@ class SensorStateModel {
     final ByteData buffer = ByteData.view(Uint8List.fromList(data).buffer);
     int identifier = buffer.getInt16(0);
     int crc = 0;
-    _startLeftTimer();
-
     final DateTime now = DateTime.now();
-    switch (identifier) {
-      case 0x0103:
+    switch (side) {
+      case Side.left:
         {
+          _startLeftTimer();
+          switch (identifier) {
+            case 0x0103:
+              {
+                _leftValues = SensorValues(time: now, data: [], side: 'LEFT');
+                for (int i = 1; i < buffer.lengthInBytes / 2; i++) {
+                  _leftValues?.data.add(buffer.getInt16((i * 2)));
+                }
+
+                if ((_leftValues?.data.length ?? 0) > 12) {
+                  crc = _leftValues?.data.last ?? 0;
+                  _leftValues?.data.removeLast();
+                }
+                break;
+              }
+            default:
+              {
+                if (_leftValues?.data.isNotEmpty ?? false) {
+                  for (int i = 0; i < buffer.lengthInBytes / 2; i++) {
+                    _leftValues?.data.add(buffer.getInt16((i * 2)));
+                  }
+
+                  if ((_leftValues?.data.length ?? 0) > 12) {
+                    crc = _leftValues?.data.last ?? 0;
+                    _leftValues?.data.removeLast();
+                  }
+                }
+              }
+          }
+        }
+        if (crc != 0 && _leftValues?.data.length == 12) {
+          _leftValuesStream.add(_leftValues!);
+          _leftCounter++;
+        }
+      case Side.right:
+        {
+          _startRightTimer();
+          switch (identifier) {
+            case 0x0203:
+              {
+                _rightValues = SensorValues(time: now, data: [], side: 'RIGHT');
+                for (int i = 1; i < buffer.lengthInBytes / 2; i++) {
+                  _rightValues?.data.add(buffer.getInt16((i * 2)));
+                }
+
+                if ((_rightValues?.data.length ?? 0) > 12) {
+                  crc = _rightValues?.data.last ?? 0;
+                  _rightValues?.data.removeLast();
+                }
+                break;
+              }
+            default:
+              {
+                if (_rightValues?.data.isNotEmpty ?? false) {
+                  for (int i = 0; i < buffer.lengthInBytes / 2; i++) {
+                    _rightValues?.data.add(buffer.getInt16((i * 2)));
+                  }
+
+                  if ((_rightValues?.data.length ?? 0) > 12) {
+                    crc = _rightValues?.data.last ?? 0;
+                    _rightValues?.data.removeLast();
+                  }
+                }
+              }
+          }
+          if (crc != 0 && _rightValues?.data.length == 12) {
+            _rightValuesStream.add(_rightValues!);
+            _rightCounter++;
+          }
+        }
+    }
+  }
+
+  updateSaltedValues(List<int> data, Side side) {
+    final ByteData buffer = ByteData.sublistView(
+      Uint8List.fromList(data),
+    );
+    final DateTime now = DateTime.now();
+    final int numInt16Values = buffer.lengthInBytes ~/ 4;
+    switch (side) {
+      case Side.left:
+        {
+          _startLeftTimer();
           _leftValues = SensorValues(time: now, data: [], side: 'LEFT');
-          for (int i = 1; i < buffer.lengthInBytes / 2; i++) {
-            _leftValues?.data.add(buffer.getInt16((i * 2)));
+          for (int i = 2; i < numInt16Values - 1; i++) {
+            _leftValues?.data.add(
+              buffer.getInt32(i * 4, Endian.little),
+            );
           }
-
-          if ((_leftValues?.data.length ?? 0) > 12) {
-            crc = _leftValues?.data.last ?? 0;
-            _leftValues?.data.removeLast();
-          }
-          break;
+          _leftValuesStream.add(_leftValues!);
+          _leftCounter++;
         }
-      default:
+      case Side.right:
         {
-          if (_leftValues?.data.isNotEmpty ?? false) {
-            for (int i = 0; i < buffer.lengthInBytes / 2; i++) {
-              _leftValues?.data.add(buffer.getInt16((i * 2)));
-            }
-
-            if ((_leftValues?.data.length ?? 0) > 12) {
-              crc = _leftValues?.data.last ?? 0;
-              _leftValues?.data.removeLast();
-            }
-          }
-        }
-    }
-    if (crc != 0 && _leftValues?.data.length == 12) {
-      _leftValuesStream.add(_leftValues!);
-      _leftCounter++;
-    }
-  }
-
-  updateLeft4Values(List<int> data) {
-    final ByteData buffer = ByteData.sublistView(Uint8List.fromList(data));
-    _startLeftTimer();
-    final DateTime now = DateTime.now();
-    _leftValues = SensorValues(time: now, data: [], side: 'LEFT');
-    final int numInt16Values = buffer.lengthInBytes ~/ 4;
-    for (int i = 2; i < numInt16Values - 1; i++) {
-      _leftValues?.data.add(buffer.getInt32(i * 4, Endian.little));
-    }
-    _leftValuesStream.add(_leftValues!);
-    _leftCounter++;
-  }
-
-  updateRight12Values(List<int> data) {
-    final ByteData buffer = ByteData.view(Uint8List.fromList(data).buffer);
-    int identifier = buffer.getInt16(0);
-    int crc = 0;
-    _startRightTimer();
-
-    final DateTime now = DateTime.now();
-    switch (identifier) {
-      case 0x0203:
-        {
+          _startRightTimer();
           _rightValues = SensorValues(time: now, data: [], side: 'RIGHT');
-          for (int i = 1; i < buffer.lengthInBytes / 2; i++) {
-            _rightValues?.data.add(buffer.getInt16((i * 2)));
+          for (int i = 2; i < numInt16Values - 1; i++) {
+            _rightValues?.data.add(
+              buffer.getInt32(i * 4, Endian.little),
+            );
           }
-
-          if ((_rightValues?.data.length ?? 0) > 12) {
-            crc = _rightValues?.data.last ?? 0;
-            _rightValues?.data.removeLast();
-          }
-          break;
-        }
-      default:
-        {
-          if (_rightValues?.data.isNotEmpty ?? false) {
-            for (int i = 0; i < buffer.lengthInBytes / 2; i++) {
-              _rightValues?.data.add(buffer.getInt16((i * 2)));
-            }
-
-            if ((_rightValues?.data.length ?? 0) > 12) {
-              crc = _rightValues?.data.last ?? 0;
-              _rightValues?.data.removeLast();
-            }
-          }
+          _rightValuesStream.add(_rightValues!);
+          _rightCounter++;
         }
     }
-    if (crc != 0 && _rightValues?.data.length == 12) {
-      _rightValuesStream.add(_rightValues!);
-      _rightCounter++;
-    }
-  }
-
-  updateRight4Values(List<int> data) {
-    final ByteData buffer = ByteData.sublistView(Uint8List.fromList(data));
-    _startRightTimer();
-    final DateTime now = DateTime.now();
-    _rightValues = SensorValues(time: now, data: [], side: 'RIGHT');
-    final int numInt16Values = buffer.lengthInBytes ~/ 4;
-    for (int i = 2; i < numInt16Values - 1; i++) {
-      _rightValues?.data.add(buffer.getInt32(i * 4, Endian.little));
-    }
-    _rightValuesStream.add(_rightValues!);
-    _rightCounter++;
   }
 
   void _normalizeValues(SensorValues values, EventSink<List<double>> sink) {
