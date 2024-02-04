fcf38deb78c3e22ef459dde5c2c08799faaafb9b
Hannes Seidel <greenredact@gmail.com>
2024-02-04 18:39:35 +0100

    fixed salted values encoding

--- lib/models/sensor_state_model.dart
+++ lib/models/sensor_state_model.dart
@@ -39,37 +39,27 @@ class SensorStateModel {
 
   Timer? _leftTimer;
   Timer? _rightTimer;
-  void _startLeftTimer() {
-    _leftTimer ??= Timer.periodic(
-      const Duration(seconds: 1),
-      (Timer timer) {
-        _leftFrequencyStream.add(_leftCounter);
-        _leftCounter = 0;
-      },
-    );
-  }
-
-  void _startRightTimer() {
-    _rightTimer ??= Timer.periodic(
-      const Duration(seconds: 1),
-      (Timer timer) {
-        _rightFrequencyStream.add(_rightCounter);
-        _rightCounter = 0;
-      },
-    );
-  }
-
   updateFsrtecValues(List<int> data, Side side) {
+    final buffer = ByteData.view(Uint8List.fromList(data).buffer);
+    final time = DateTime.now();
+    _parseFsrtecValues(buffer, time, side);
+  }
+
+  updateSaltedValues(List<int> data, Side side) {
+    final buffer = ByteData.sublistView(Uint8List.fromList(data));
+    final time = DateTime.now();
+    _parseSaltedValues(buffer, time, side);
+  }
+
+  void _parseFsrtecValues(ByteData buffer, DateTime time, Side side) {
     // assumptions:
     // - package length is always multiple of 2 (List of uint16 big endian)
     // - full package has 28 bytes
     // - first value is identifier
     // - 12 sensor values are split on two packages
     // - last value is crc
-    final ByteData buffer = ByteData.view(Uint8List.fromList(data).buffer);
     int identifier = buffer.getInt16(0);
     int crc = 0;
-    final DateTime now = DateTime.now();
     switch (side) {
       case Side.left:
         {
@@ -78,7 +68,7 @@ class SensorStateModel {
             case 0x0103:
               {
                 _leftValues = SensorValues(
-                  time: now,
+                  time: time,
                   data: [],
                   side: side.description.toUpperCase(),
                 );
@@ -121,7 +111,7 @@ class SensorStateModel {
             case 0x0203:
               {
                 _rightValues = SensorValues(
-                  time: now,
+                  time: time,
                   data: [],
                   side: side.description.toUpperCase(),
                 );
@@ -160,25 +150,30 @@ class SensorStateModel {
     }
   }
 
-  updateSaltedValues(List<int> data, Side side) {
-    final ByteData buffer = ByteData.sublistView(
-      Uint8List.fromList(data),
-    );
-    final DateTime now = DateTime.now();
-    final int numInt16Values = buffer.lengthInBytes ~/ 4;
+  _parseSaltedValues(ByteData buffer, DateTime time, Side side) {
     switch (side) {
       case Side.left:
         {
           _startLeftTimer();
           _leftValues = SensorValues(
-            time: now,
+            time: time,
             data: [],
             side: side.description.toUpperCase(),
           );
-          for (int i = 2; i < numInt16Values - 1; i++) {
-            _leftValues?.data.add(
-              buffer.getInt32(i * 4, Endian.little),
-            );
+          // experimentally found four 10bit sensor values at these offsets
+          for (int i = 9; i < 17; i += 2) {
+            if (i & 0x2 != 0) {
+              // for every second sensor value ...
+              _leftValues?.data.add(
+                0x3ff - // ... subtract from max 10 bit
+                    (buffer.getUint16(i, Endian.little) &
+                        0x3ff), // mask low 10 bits
+              );
+            } else {
+              _leftValues?.data.add(
+                buffer.getUint16(i, Endian.little) & 0x03ff, // mask low 10 bits
+              );
+            }
           }
           _leftValues?.normalized = _deviceSelector.normalizeData(
             _leftValues!.data,
@@ -186,18 +181,30 @@ class SensorStateModel {
           _leftValuesStream.add(_leftValues!);
           _leftCounter++;
         }
+
       case Side.right:
         {
           _startRightTimer();
           _rightValues = SensorValues(
-            time: now,
+            time: time,
             data: [],
             side: side.description.toUpperCase(),
           );
-          for (int i = 2; i < numInt16Values - 1; i++) {
-            _rightValues?.data.add(
-              buffer.getInt32(i * 4, Endian.little),
-            );
+
+          // experimentally found four 10bit sensor values at these offsets
+          for (int i = 9; i < 17; i += 2) {
+            if (i & 0x2 != 0) {
+              // for every second sensor value ...
+              _rightValues?.data.add(
+                0x3ff - // ... subtract from max 10 bit
+                    (buffer.getUint16(i, Endian.little) &
+                        0x3ff), // mask low 10 bits
+              );
+            } else {
+              _rightValues?.data.add(
+                buffer.getUint16(i, Endian.little) & 0x03ff, // mask low 10 bits
+              );
+            }
           }
           _rightValues?.normalized = _deviceSelector.normalizeData(
             _rightValues!.data,
@@ -206,5 +213,25 @@ class SensorStateModel {
           _rightCounter++;
         }
     }
+  }
+
+  void _startLeftTimer() {
+    _leftTimer ??= Timer.periodic(
+      const Duration(seconds: 1),
+      (Timer timer) {
+        _leftFrequencyStream.add(_leftCounter);
+        _leftCounter = 0;
+      },
+    );
+  }
+
+  void _startRightTimer() {
+    _rightTimer ??= Timer.periodic(
+      const Duration(seconds: 1),
+      (Timer timer) {
+        _rightFrequencyStream.add(_rightCounter);
+        _rightCounter = 0;
+      },
+    );
   }
 }
