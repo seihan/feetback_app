b5817a4ea4ea1c44fbc1fa6cef7eb0f970ff1101
Hannes Seidel <greenredact@gmail.com>
2024-05-17 09:49:15 +0200

    updated packages - replaced deprecated charts package - clean up pubspec yaml

--- lib/widgets/sensor_chart.dart
+++ lib/widgets/sensor_chart.dart
@@ -1,4 +1,5 @@
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:community_charts_flutter/community_charts_flutter.dart'
+    as charts;
 import 'package:flutter/material.dart';
 
 import '../generated/l10n.dart';
